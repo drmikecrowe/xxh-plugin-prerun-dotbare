d2730959b32fc54e03febd779b6cef6df13ec96f
Mike Crowe <drmikecrowe@gmail.com>
2022-02-13 11:31:18 -0500

    getting a bit more standardized

--- 100-prerun.sh
+++ 100-prerun.sh
@@ -1,18 +1,23 @@
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+plugin_name="$(basename $(dirname $(dirname ${BASH_SOURCE[0]})))"
+BASH_IT="$CURR_DIR/.bash_it"
+LOGFILE=/tmp/$plugin_name.log
+
 if [[ $XDG_CACHE_HOME ]]; then
-  done_file=$XDG_CACHE_HOME/xxh-plugin-prerun-dotbare-done
+  done_file=$XDG_CACHE_HOME/${plugin_name}-done
 else
-  done_file=$XXH_HOME/.xxh-plugin-prerun-dotbare-done
+  done_file=$XXH_HOME/.${plugin_name}-done
 fi
 
 if [[ ! -f $done_file ]]; then
   if [[ ! -z $DOTBARE_REPO ]]; then
     export DOTBARE_DIR="$XXH_HOME/.dotbarefiles"
     if [[ $XXH_VERBOSE == '1' || $XXH_VERBOSE == '2' ]]; then
-        echo "xxh-plugin-prerun-dotbare: installing $DOTBARE_REPO"
+        echo "${plugin_name}: installing $DOTBARE_REPO"
     fi
-    wget https://github.com/kazhala/dotbare/archive/refs/heads/master.tar.gz -qO- | tar -xz && mv dotbare-master $XXH_HOME/.dotbare && \
+    wget https://github.com/kazhala/dotbare/archive/refs/heads/master.tar.gz -qO- | tar -xz && mv dotbare-master $XXH_HOME/.dotbare >> $LOGFILE 2>&1 && \
         source $XXH_HOME/.dotbare/dotbare.plugin.bash && \
-        dotbare finit -u https://github.com/$DOTBARE_REPO
+        dotbare finit -u https://github.com/$DOTBARE_REPO >> $LOGFILE 2>&1
     mkdir -p `dirname $done_file`
     echo 'done' > $done_file
   fi
